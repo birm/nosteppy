d23661f06bd8efa36905b5d018285a9a12b41fd4
Ryan Birmingham <birm@rbirm.us>
2020-10-22 09:21:00 +0400

    add base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ExplorerComponent } from './explorer/explorer.component';
+import { VisualizerComponent } from './visualizer/visualizer.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    ExplorerComponent,
+    VisualizerComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
